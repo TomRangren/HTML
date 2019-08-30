b24a67b21c7d9b0779cfc2a5ede98dfb5cc7525e
TomRangren <54435262+TomRangren@users.noreply.github.com>
2019-08-30 13:02:42 +0200

    Create uppg 2html.cs

---
+++ uppg 2html.cs
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<html lang="sv">
+<head>
+<title>Uppg1</title>
+    <link href="Style.CSS" rel ="Stylesheet">
+</head>
+<body bgcolor="white">
+<h1> Tom Rangren </h1>
+<p class="Program3" > Jag är 17 år och bor i en lite by som heter Grimslöv. <br> Jag har en lillebror som heter Max som är 15 år och går i nionde klass.
+</p>
+<h2> Mina intressen </h2>
+<p> Mina intressen är datorer och teknik som ämne. Jag har även ett stort intresse för datorspel och hur datorer är uppbyggda. </p>
+<h2> Teknikprogramet </h2>
+<p class= "Program" > Teknikprogramet fokuserar sig på teknik och elekronik och jag valde programet då mina intressen är teknik och programering. </p>
+<p class= "Program2" > Jag tycker kursen är väldigt intressant och jag vill lära mig lite mer om programering. </p>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPOxJsuHu9JT8dStxgssKLFwh6uXbmX4Gz8mGg13nCk6KxbYcE" alt=Bild på teknikum>
+</img>
+    <br>
+  <a href="https://www.vaxjo.se/sidor/forskola-och-skola/gymnasieskola/teknikum.html">
+     klicka mig</a> 
+</body>
+</html> 
